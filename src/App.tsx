da42e5a6336549d55873cb53cba27a9c4dfd9c8d
Aladdin Alizada <tu7c5zneb@code.edu.az>
2024-10-30 15:15:15 +0400

    complete Header and router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import "./App.css";
 import Footer from "./layout/footer/footer";
 import Header from "./layout/header/header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+import Services from "./pages/Services";
+import Blog from "./pages/Blog";
 
 function App() {
   return (
     <>
-      <div>
+      <BrowserRouter>
         <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
         <Footer />
-      </div>
+      </BrowserRouter>
     </>
   );
 }
--- src/layout/header/header.tsx
+++ src/layout/header/header.tsx
@@ -1,7 +1,64 @@
-import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useState } from "react";
+import "./index.css";
 
 const Header = () => {
-  return <div>header</div>;
+  const location = useLocation();
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Toggle menu visibility
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="headerContainer">
+      <div className="left">
+        <h1>LOGO</h1>
+      </div>
+      <div className="right">
+        <button className="hamburger" onClick={toggleMenu}>
+          ☰
+        </button>
+
+        <nav className={`navbar ${isOpen ? "open" : ""}`}>
+          <ul className="navlist">
+            <Link
+              to="/"
+              className={location.pathname === "/" ? "active" : ""}
+              onClick={toggleMenu}
+            >
+              Home
+            </Link>
+            <Link
+              to="/about"
+              className={location.pathname === "/about" ? "active" : ""}
+              onClick={toggleMenu}
+            >
+              About us
+            </Link>
+            <Link
+              to="/services"
+              className={location.pathname === "/services" ? "active" : ""}
+              onClick={toggleMenu}
+            >
+              Services
+            </Link>
+            <Link
+              to="/blog"
+              className={location.pathname === "/blog" ? "active" : ""}
+              onClick={toggleMenu}
+            >
+              Blog
+            </Link>
+            <li>
+              <button className="contactBtn">Contact us</button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
