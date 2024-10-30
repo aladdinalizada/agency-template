439e67537c936b7e600434f2a75f3f29952eaef1
Aladdin Alizada <tu7c5zneb@code.edu.az>
2024-10-30 12:28:36 +0400

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Footer from "./layout/footer/footer";
+import Header from "./layout/header/header";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Header />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
